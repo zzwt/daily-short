815880e7ee22d7fd78f755d34ba5508bd7ab978c
zzwt <loophone@gmail.com>
2021-04-06 17:56:23 +1000

    Import global style with styled-components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
-import './global.css';
+import { createGlobalStyle } from 'styled-components';
 import Header from '../components/header';
-// This default export is required in a new `pages/_app.js` file.
 import { SWRConfig } from 'swr';
 import axios from 'axios';
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URI;
@@ -19,8 +18,51 @@ export default function MyApp({ Component, pageProps }) {
         },
       }}
     >
+      <GlobalStyle />
       <Header></Header>
       <Component {...pageProps} />
     </SWRConfig>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', 'Helvetica', 'Arial', sans-serif;
+    padding: 0;
+    /* max-width: 680px; */
+    margin: 0 auto;
+    background-image: linear-gradient(
+      to bottom right,
+      rgb(151, 153, 183) 10%,
+      rgb(80, 81, 110) 80%
+    );
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    /* min-height: 100vh; */
+    box-sizing: border-box;
+    /* color: #474747; */
+    color: rgb(65, 79, 118);
+  }
+
+  .wrapper {
+    width: 1000px;
+    margin: 0 auto;
+  }
+
+  .border-shadow {
+    border-radius: 5px;
+    background: white;
+    /* border: 1px solid #dedede; */
+    box-shadow: 1px 1px 5px rgb(151, 153, 183);
+  }
+
+  .spinner {
+    width: 50px;
+    height: 50px;
+    margin: 0px auto;
+  }
+
+  .message {
+    margin-top: 20px;
+  }
+`;
